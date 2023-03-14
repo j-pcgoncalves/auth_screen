ac417e19b23ae455b66f63030e03ccc4b7439535
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-03-14 20:34:07 +0000

    add title and button

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Auth Screen',
       theme: ThemeData(
+        brightness: Brightness.dark,
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: const TextTheme(
+          displayMedium: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          labelLarge: TextStyle(
+            color: kPrimaryColor,
+          ),
+          headlineSmall: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
       ),
-      home: WelcomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
@@ -30,14 +44,66 @@ class WelcomeScreen extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Expanded(
+            flex: 3,
             child: Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/perosn.jpeg'),
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'BAKING LESSONS\n',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      TextSpan(
+                        text: 'MASTER THE ART OF BAKING',
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      )
+                    ],
+                  ),
+                ),
+                FittedBox(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 26,
+                      vertical: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: kPrimaryColor,
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'START LEARNING',
+                          style:
+                              Theme.of(context).textTheme.labelLarge?.copyWith(
+                                    color: Colors.black,
+                                  ),
+                        ),
+                        const SizedBox(width: 10),
+                        const Icon(
+                          Icons.arrow_forward,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
